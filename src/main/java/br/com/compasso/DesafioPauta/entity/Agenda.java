38fc38b79c3883a309aa56783cd6b614f6c3298d
Matheus Edison Barkert <matheus.barkert@pampa.compasso>
2020-05-29 15:49:14 -0300

    adding builder to agenda

--- src/main/java/br/com/compasso/DesafioPauta/entity/Agenda.java
+++ src/main/java/br/com/compasso/DesafioPauta/entity/Agenda.java
@@ -3,6 +3,8 @@ package br.com.compasso.DesafioPauta.entity;
 import br.com.compasso.DesafioPauta.dto.entry.AgendaEntry;
 import br.com.compasso.DesafioPauta.enumeration.AgendaStatus;
 import br.com.compasso.DesafioPauta.enumeration.VoteResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,6 +15,8 @@ import java.time.LocalDateTime;
 @Data
 @Document
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Agenda {
 
     @Id
