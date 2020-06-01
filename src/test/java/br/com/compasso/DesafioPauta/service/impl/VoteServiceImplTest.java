8f26052e3695a5a20d5824c90281a0a573d3c1c1
Matheus Edison Barkert <matheus.barkert@pampa.compasso>
2020-06-01 13:26:49 -0300

    test of voteServiceImpl

---
+++ src/test/java/br/com/compasso/DesafioPauta/service/impl/VoteServiceImplTest.java
@@ -0,0 +1,93 @@
+package br.com.compasso.DesafioPauta.service.impl;
+
+import br.com.compasso.DesafioPauta.entity.Agenda;
+import br.com.compasso.DesafioPauta.entity.Associated;
+import br.com.compasso.DesafioPauta.entity.Vote;
+import br.com.compasso.DesafioPauta.enumeration.AgendaStatus;
+import br.com.compasso.DesafioPauta.enumeration.VoteResponse;
+import br.com.compasso.DesafioPauta.repository.VoteRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class VoteServiceImplTest {
+
+    @Mock
+    private VoteRepository voteRepository;
+
+    @Mock
+    private AgendaServiceImpl agendaService;
+
+    @Mock
+    private AssociatedServiceImpl associatedService;
+
+    @InjectMocks
+    private VoteServiceImpl voteService;
+
+    @Test
+    void listTest() {
+
+        var vote = generateVote();
+        var votes = Arrays.asList(vote);
+        when(voteRepository.findAll()).thenReturn(votes);
+        var voteList = voteService.list();
+        assertEquals(votes, voteList);
+        verify(voteRepository).findAll();
+    }
+
+    @Test
+    void registerTest() {
+        var vote = generateVote();
+
+        var vote2 = Vote.builder()
+                .id("2")
+                .agenda(Agenda.builder().id("2").status(AgendaStatus.OPEN).build())
+                .associated(Associated.builder().id("2").build())
+                .response(VoteResponse.YES)
+                .build();
+
+        when(voteRepository.save(vote)).thenReturn(vote);
+        when(agendaService.find(eq(vote.getAgenda().getId()))).thenReturn(vote.getAgenda());
+        when(associatedService.find(eq(vote.getAssociated().getId()))).thenReturn(vote.getAssociated());
+        when(voteService.list()).thenReturn(Arrays.asList(vote2));
+        Vote register = voteService.register(vote);
+        assertEquals(vote, register);
+        verify(voteRepository).save(vote);
+    }
+
+    private Vote generateVote() {
+
+        Agenda agenda = Agenda.builder()
+                .id("1")
+                .title("title")
+                .description("description")
+                .status(AgendaStatus.OPEN)
+                .begin(LocalDateTime.now())
+                .build();
+
+        Associated associated = Associated.builder()
+                .id("1")
+                .name("Matheus")
+                .email("matheus@mail.com")
+                .build();
+
+        return Vote.builder()
+                .id("1")
+                .agenda(agenda)
+                .associated(associated)
+                .response(VoteResponse.YES)
+                .build();
+    }
+}
